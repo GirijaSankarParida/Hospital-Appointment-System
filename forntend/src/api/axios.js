357761724cf2b08a9c067da004d1393fdf6ed85c
GirijaSankarParida <gsankarparida@gmail.com>
2026-08-04 12:29:57 +0530

    Add files via upload

---
+++ forntend/src/api/axios.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:5000/api",
+});
+
+// Attach the JWT token (if present) to every request
+api.interceptors.request.use((config) => {
+  const userInfo = localStorage.getItem("userInfo");
+  if (userInfo) {
+    const { token } = JSON.parse(userInfo);
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
